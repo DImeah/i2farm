7cc14066418e3be57097719a7fda829fa16552d7
Imeah <imeahdornu@gmail.com>
2025-03-27 18:10:32 +0100

    made changes to the header in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ThemeProvider from "../../context/Theme";
 // import Navbar from "@/components/navigation/navbar";
-import Header from "@/components/Header";
+// import Header from "@/components/Header";
 import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
